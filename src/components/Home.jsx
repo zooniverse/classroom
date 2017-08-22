b27453d3d8a2d11a206b88941a985192589dbfc5
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-08-22 21:13:53 +0100

    Basic carded home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Section from 'grommet/components/Section';
+import Card from 'grommet/components/Card';
 import Anchor from 'grommet/components/Anchor';
+import Tiles from 'grommet/components/Tiles';
+import Tile from 'grommet/components/Tile';
+
 import { Actions } from 'jumpstate';
 import { Link } from 'react-router-dom';
 import { config } from '../lib/config';
+
+import thumbnailAstro from '../images/home-card-intro-to-astro.jpg';
+import thumbnailDarien from '../images/home-card-wildcam-darien.jpg';
 
 // TODO: Look into grommet's path prop for buttons to see if it works with React-Router v4
 export default function Home() {
@@ -13,9 +20,40 @@ export default function Home() {
 
   return (
     <Section colorIndex="light-2">
-      <Anchor path="/astro" onClick={handleSettingProjectCollection.bind(null, config.astroProjects)}>Introduction to Astronomy</Anchor>
-      <Anchor path="/" onClick={handleSettingProjectCollection.bind(null, [])}>Wildcam Darian: Labs</Anchor>
-      <Anchor path="/tester" onClick={handleSettingProjectCollection.bind(null, [])}>Tester</Anchor>
+      <Tiles
+        fill={true}
+        flush={false}
+      >
+        <Tile>
+          <Card
+            thumbnail={thumbnailAstro}
+            heading="Introduction to Astronomy"
+            description="Classroom tools for teaching Astronomy."
+            link={
+              <Anchor path="/astro" onClick={handleSettingProjectCollection.bind(null, config.astroProjects)}>Enter</Anchor>
+            }
+          />
+        </Tile>
+        <Tile>
+          <Card
+            thumbnail={thumbnailDarien}
+            heading="WildCam Darien Lab"
+            description="A map for exploring camera trap data from the WildCam Darien project."
+            link={
+              <Anchor path="/" onClick={handleSettingProjectCollection.bind(null, [])}>Enter</Anchor>
+            }
+          />
+        </Tile>
+        <Tile>
+          <Card
+            heading="Experimental Pages"
+            description="Features that are still in testing. Safety not guaranteed."
+            link={
+              <Anchor path="/tester" onClick={handleSettingProjectCollection.bind(null, [])}>Enter</Anchor>
+            }
+          />
+        </Tile>
+      </Tiles>
     </Section>
   );
 };
