26dd9e655a4c57f112120aee91c6238630f4fdba
Sarah Allen <sarah@zooniverse.org>
2017-09-13 15:56:52 -0500

    Darien routes

--- src/components/astro/AstroHome.jsx
+++ src/components/astro/AstroHome.jsx
@@ -13,7 +13,6 @@ import Paragraph from 'grommet/components/Paragraph';
 
 import ProgramHome from '../common/ProgramHome';
 import NeedHelp from '../common/NeedHelp';
-
 import ClassroomsLayout from '../../components/classrooms/ClassroomsLayout';
 import {
   PROGRAMS_INITIAL_STATE, PROGRAMS_PROPTYPES, PROGRAMS_STATUS
