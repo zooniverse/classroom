1d06b7d561dda7e73bf03501be16d1dea6030b8e
Sarah Allen <sarah@zooniverse.org>
2017-11-02 12:09:05 -0500

    Install dev version

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,8 @@ module.exports = {
 
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.css', '.styl'],
-    modules: ['.', 'node_modules']
+    modules: ['.', 'node_modules'],
+    symlinks: false
   },
 
   module: {
