508f5aba4d6a3fdab769c77dc88b951fbe317435
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-12-06 18:08:12 +0000

    Add basic Darien Classrooms

--- src/components/darien/DarienEducators.jsx
+++ src/components/darien/DarienEducators.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ClassroomsLayout from '../classrooms/ClassroomsLayout';
 
 const DarienEducators = (props) => {
   return (
-    <div>DARIEN EDUCATORS PLACEHOLDER</div>
+    <ClassroomsLayout match={props.match} />
   );
 };
 
 DarienEducators.defaultProps = {};
-
 DarienEducators.propTypes = {};
 
 export default DarienEducators;
