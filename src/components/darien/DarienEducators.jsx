9e313e9322ba0866db20a9331e7192f93f9b0c54
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2017-12-18 17:56:06 +0000

    Copied ClassroomLayout to DarienEducators

--- src/components/darien/DarienEducators.jsx
+++ src/components/darien/DarienEducators.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import ClassroomsLayout from '../classrooms/ClassroomsLayout';
+import { connect } from 'react-redux';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { Actions } from 'jumpstate';
+import Box from 'grommet/components/Box';
+import Toast from 'grommet/components/Toast';
 
-function DarienEducators(props) {
+import ClassroomsManagerContainer from '../../containers/classrooms/ClassroomsManagerContainer';
+import ClassroomEditorContainer from '../../containers/classrooms/ClassroomEditorContainer';
+
+import {
+  CLASSROOMS_INITIAL_STATE, CLASSROOMS_PROPTYPES
+} from '../../ducks/classrooms';
+
+function DarienEducators({ classroomInstructions, match, toast }) {
   return (
-    <ClassroomsLayout match={props.match} />
+    <Box
+      className="classrooms-layout"
+      direction="column"
+      colorIndex="grey-5"
+      full={{ horizontal: true, vertical: false }}
+      pad="large"
+    >
+      {toast && toast.message &&
+        <Toast
+          status={toast.status ? toast.status : 'unknown'}
+          onClose={() => { Actions.classrooms.setToastState({ status: null, message: null }); }}
+        >
+          {toast.message}
+        </Toast>}
+      <Switch>
+        <Route exact path={`${match.url}/classrooms/:id`} component={ClassroomEditorContainer} />
+        <Route path={`${match.url}`} component={ClassroomsManagerContainer} classroomInstructions={classroomInstructions} />
+      </Switch>
+    </Box>
   );
 };
 
-DarienEducators.defaultProps = {};
-DarienEducators.propTypes = {};
+DarienEducators.defaultProps = {
+  ...CLASSROOMS_INITIAL_STATE
+};
 
-export default DarienEducators;
+DarienEducators.propTypes = {
+  ...CLASSROOMS_PROPTYPES
+};
+
+function mapStateToProps(state) {
+  return {
+    toast: state.classrooms.toast
+  };
+}
+
+export default connect(mapStateToProps)(DarienEducators);
