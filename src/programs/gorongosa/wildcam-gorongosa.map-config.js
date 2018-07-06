9857439d31487009fe93a5a7cd4af32403dd46a2
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-07-06 15:45:08 +0100

    Fixed Gorongosa map for selecting assignments

--- src/programs/gorongosa/wildcam-gorongosa.map-config.js
+++ src/programs/gorongosa/wildcam-gorongosa.map-config.js
@@ -25,13 +25,13 @@ const mapConfig = {
       'selectCameraCount': 'SELECT cam.*, COUNT(sbjagg.*) as count FROM wildcam_gorongosa_cameras_201601 AS cam LEFT JOIN (SELECT sbj.camera, sbj.location, sbj.dateutc, sbj.season, sbj.time_period, agg.species, agg.subject_id FROM wildcam_gorongosa_subjects_201601_16000 AS sbj INNER JOIN wildcam_gorongosa_aggregations_201603a AS agg ON sbj.subject_id = agg.subject_id) AS sbjagg ON cam.id = sbjagg.camera {WHERE} GROUP BY cam.cartodb_id ORDER BY count DESC',
       
       //Get all the details for all the (filtered) results.
-      'selectForDownload': 'SELECT cam.*, sbjagg.* FROM wildcam_gorongosa_cameras_201601 AS cam INNER JOIN (SELECT sbj.camera, sbj.location, sbj.month, sbj.year, sbj.season, sbj.time_period, sbj.time, sbj.dateutc, sbj.darien_id, agg.species, agg.data_answers_howmany_1, agg.data_answers_howmany_2, agg.data_answers_howmany_3, agg.data_answers_howmany_4, agg.data_answers_howmany_5, agg.data_answers_howmany_6, agg.data_answers_howmany_7, agg.data_answers_howmany_8, agg.data_answers_howmany_9, agg.data_answers_howmany_10, agg.data_answers_howmany_1120, agg.data_answers_howmany_21 FROM wildcam_gorongosa_subjects_201601_16000 AS sbj INNER JOIN wildcam_gorongosa_aggregations_201603a AS agg ON sbj.subject_id = agg.subject_id) AS sbjagg ON cam.id = sbjagg.camera {WHERE}',
+      'selectForDownload': 'SELECT cam.*, sbjagg.* FROM wildcam_gorongosa_cameras_201601 AS cam INNER JOIN (SELECT sbj.camera, sbj.location, sbj.month, sbj.year, sbj.season, sbj.time_period, sbj.timeutc, sbj.dateutc, sbj.gorongosa_id, agg.species, agg.num_classifications FROM wildcam_gorongosa_subjects_201601_16000 AS sbj INNER JOIN wildcam_gorongosa_aggregations_201603a AS agg ON sbj.subject_id = agg.subject_id) AS sbjagg ON cam.id = sbjagg.camera {WHERE}',
       
       //Get all the minimum Subject details for all the (filtered) results. Has Order By and Limit clauses.
-      'selectForAssignment': 'SELECT sbjagg.subject_id, sbjagg.location FROM wildcam_gorongosa_cameras_201601 AS cam INNER JOIN (SELECT sbj.subject_id, sbj.camera, sbj.location, sbj.month, sbj.year, sbj.season, sbj.time_period, sbj.time, sbj.dateutc, sbj.darien_id, agg.species, agg.data_answers_howmany_1, agg.data_answers_howmany_2, agg.data_answers_howmany_3, agg.data_answers_howmany_4, agg.data_answers_howmany_5, agg.data_answers_howmany_6, agg.data_answers_howmany_7, agg.data_answers_howmany_8, agg.data_answers_howmany_9, agg.data_answers_howmany_10, agg.data_answers_howmany_1120, agg.data_answers_howmany_21 FROM wildcam_gorongosa_subjects_201601_16000 AS sbj INNER JOIN wildcam_gorongosa_aggregations_201603a AS agg ON sbj.subject_id = agg.subject_id) AS sbjagg ON cam.id = sbjagg.camera {WHERE} {ORDER} {LIMIT}',      
+      'selectForAssignment': 'SELECT sbjagg.subject_id, sbjagg.location FROM wildcam_gorongosa_cameras_201601 AS cam INNER JOIN (SELECT sbj.subject_id, sbj.camera, sbj.location, sbj.month, sbj.year, sbj.season, sbj.time_period, sbj.timeutc, sbj.dateutc, sbj.gorongosa_id, agg.species, agg.num_classifications FROM wildcam_gorongosa_subjects_201601_16000 AS sbj INNER JOIN wildcam_gorongosa_aggregations_201603a AS agg ON sbj.subject_id = agg.subject_id) AS sbjagg ON cam.id = sbjagg.camera {WHERE} {ORDER} {LIMIT}',      
       
       //Select all the photos from a specific camera. Similar to selectForDownload
-      'selectCameraData': 'SELECT DISTINCT(sbjagg.location) FROM wildcam_gorongosa_cameras_201601 AS cam INNER JOIN (SELECT sbj.camera, sbj.location, sbj.month, sbj.year, sbj.season, sbj.time_period, sbj.time, sbj.dateutc, sbj.darien_id, agg.species FROM wildcam_gorongosa_subjects_201601_16000 AS sbj INNER JOIN wildcam_gorongosa_aggregations_201603a AS agg ON sbj.subject_id = agg.subject_id) AS sbjagg ON cam.id = sbjagg.camera {WHERE}',
+      'selectCameraData': 'SELECT DISTINCT(sbjagg.location) FROM wildcam_gorongosa_cameras_201601 AS cam INNER JOIN (SELECT sbj.camera, sbj.location, sbj.month, sbj.year, sbj.season, sbj.time_period, sbj.timeutc, sbj.dateutc, sbj.gorongosa_id, agg.species FROM wildcam_gorongosa_subjects_201601_16000 AS sbj INNER JOIN wildcam_gorongosa_aggregations_201603a AS agg ON sbj.subject_id = agg.subject_id) AS sbjagg ON cam.id = sbjagg.camera {WHERE}',
       
       //Select a single camera, mostly for the camera's metadata.
       'selectCameraMetadata': 'SELECT * FROM wildcam_gorongosa_cameras_201601 {WHERE}',
@@ -148,6 +148,210 @@ const mapConfig = {
         'label': 'Species',
         'type': 'multichoice',
         'options': [
+          {
+            'value': "Aardvark",
+            'label': "Aardvarks"
+          },
+          {
+            'value': "Baboon",
+            'label': "Baboons"
+          },
+          {
+            'value': "Bird (other)",
+            'label': "Birds (other)"
+          },
+          {
+            'value': "Bushbuck",
+            'label': "Bushbucks"
+          },
+          {
+            'value': "Bushpig",
+            'label': "Bushpigs"
+          },
+          {
+            'value': "Caracal",
+            'label': "Caracals"
+          },
+          {
+            'value': "Civet",
+            'label': "Civets"
+          },
+          {
+            'value': "Crane",
+            'label': "Cranes"
+          },
+          {
+            'value': "Duiker",
+            'label': "Duikers"
+          },
+          {
+            'value': "Eland",
+            'label': "Elands"
+          },
+          {
+            'value': "Elephant",
+            'label': "Elephants"
+          },
+          {
+            'value': "Genet",
+            'label': "Genets"
+          },
+          {
+            'value': "Ground Hornbill",
+            'label': "Ground Hornbills"
+          },
+          {
+            'value': "Hare",
+            'label': "Hares"
+          },
+          {
+            'value': "Hartebeest",
+            'label': "Hartebeests"
+          },
+          {
+            'value': "Hippopotamus",
+            'label': "Hippopotami"
+          },
+          {
+            'value': "Honey Badger",
+            'label': "Honey Badgers"
+          },
+          {
+            'value': "Hyena",
+            'label': "Hyenas"
+          },
+          {
+            'value': "Impala",
+            'label': "Impalas"
+          },
+          {
+            'value': "Jackal",
+            'label': "Jackals"
+          },
+          {
+            'value': "Kudu",
+            'label': "Kudus"
+          },
+          {
+            'value': "Leopard",
+            'label': "Leopards"
+          },
+          {
+            'value': "Lion (cub)",
+            'label': "Lion Cubs"
+          },
+          {
+            'value': "Lion (female)",
+            'label': "Lions (Females)"
+          },
+          {
+            'value': "Lion (male)",
+            'label': "Lions (Males)"
+          },
+          {
+            'value': "Mongoose",
+            'label': "Mongoose"
+          },
+          {
+            'value': "Nyala",
+            'label': "Nyalas"
+          },
+          {
+            'value': "Oribi",
+            'label': "Oribis"
+          },
+          {
+            'value': "Otter",
+            'label': "Otters"
+          },
+          {
+            'value': "Pangolin",
+            'label': "Pangolins"
+          },
+          {
+            'value': "Porcupine",
+            'label': "Porcupines"
+          },
+          {
+            'value': "Raptor (other)",
+            'label': "Raptors (other)"
+          },
+          {
+            'value': "Reedbuck",
+            'label': "Reedbucks"
+          },
+          {
+            'value': "Reptile",
+            'label': "Reptiles"
+          },
+          {
+            'value': "Rodent",
+            'label': "Rodents"
+          },
+          {
+            'value': "Sable Antelope",
+            'label': "Sable Antelopes"
+          },
+          {
+            'value': "Samango Monkey",
+            'label': "Samango Monkeys"
+          },
+          {
+            'value': "Secretary bird",
+            'label': "Secretary Birds"
+          },
+          {
+            'value': "Serval",
+            'label': "Servals"
+          },
+          {
+            'value': "Vervet Monkey",
+            'label': "Vervet Monkeys"
+          },
+          {
+            'value': "Vulture",
+            'label': "Vultures"
+          },
+          {
+            'value': "Warthog",
+            'label': "Warthogs"
+          },
+          {
+            'value': "Waterbuck",
+            'label': "Waterbucks"
+          },
+          {
+            'value': "Weasel",
+            'label': "Weasels"
+          },
+          {
+            'value': "Wildcat",
+            'label': "Wildcats"
+          },
+          {
+            'value': "Wild Dog",
+            'label': "Wild Dogs"
+          },
+          {
+            'value': "Wildebeest",
+            'label': "Wildebeest"
+          },
+          {
+            'value': "Zebra",
+            'label': "Zebras"
+          },
+          {
+            'value': "Human",
+            'label': "Humans"
+          },
+          {
+            'value': "Fire",
+            'label': "Fire!"
+          },
+          {
+            'value': "Nothing here",
+            'label': "(Nothing)"
+          }
         ]
       },
       'veg_type': {
