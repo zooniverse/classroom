a849caf563e36786009bd0efd140c8f92c11461b
Sarah Allen <sarah@zooniverse.org>
2020-03-31 15:08:52 -0500

    Add NSF logo to home page. Sort program listing

--- src/components/common/Home.jsx
+++ src/components/common/Home.jsx
@@ -3,11 +3,13 @@ import Section from 'grommet/components/Section';
 import Box from 'grommet/components/Box';
 import Card from 'grommet/components/Card';
 import Anchor from 'grommet/components/Anchor';
+import Image from 'grommet/components/Image';
 import Tiles from 'grommet/components/Tiles';
 import Tile from 'grommet/components/Tile';
 import Heading from 'grommet/components/Heading';
 import Paragraph from 'grommet/components/Paragraph';
 import Spinning from 'grommet/components/icons/Spinning';
+import nsf from '../../images/nsf.png'
 
 import { Actions } from 'jumpstate';
 import { Link } from 'react-router-dom';
@@ -21,6 +23,13 @@ import {
 
 // TODO: Look into grommet's path prop for buttons to see if it works with React-Router v4
 export default function Home(props) {
+  const sortedPrograms = props.programs.length > 0 && props.programs.sort(
+    function (programA, programB) {
+      const lowercaseProgramA = programA.name.toLowerCase()
+      const lowercaseProgramB = programB.name.toLowerCase()
+      return lowercaseProgramA.localeCompare(lowercaseProgramB);
+    }
+  ) 
   return (
     <Section className="home" colorIndex="light-2">
       <Box align="center">
@@ -42,7 +51,7 @@ export default function Home(props) {
           })}
         {props.programsStatus === PROGRAMS_STATUS.SUCCESS &&
           props.programs.length > 0 &&
-          props.programs.map((program) => {
+          sortedPrograms.map((program) => {
             const programLink = (program.metadata && program.metadata.redirect) ?
               <Anchor href={program.metadata.redirect} label="Visit Lab" /> :
               <Link to={program.slug} onClick={() => { Actions.getProgram({ programs: props.programs, param: program.slug }); }}>Enter</Link>;
@@ -59,6 +68,10 @@ export default function Home(props) {
             );
           })}
       </Tiles>
+      <hr />
+      <Box direction='row' justify='center'>
+        <Image alt='National Science Foundation' src='../../images/nsf.png' size='small' />
+      </Box>
     </Section>
   );
 }
