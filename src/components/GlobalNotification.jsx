f9cc3623b2f8fa68c64d28a16c30b5631a6160a4
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-11-25 22:42:55 +0000

    Add text for Global Notification, re: sign in issue

--- src/components/GlobalNotification.jsx
+++ src/components/GlobalNotification.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Anchor from 'grommet/components/Anchor';
 import Box from 'grommet/components/Box';
 import Button from 'grommet/components/Button';
 import Label from 'grommet/components/Label';
 import Paragraph from 'grommet/components/Paragraph';
+
+import AlertIcon from 'grommet/components/icons/base/Alert';
 import CloseIcon from 'grommet/components/icons/base/Close';
 import DownIcon from 'grommet/components/icons/base/Down';
 
@@ -32,7 +35,14 @@ class GlobalNotification extends React.Component {
           direction="row"
           pad="none"
         >
-          <Label size="small">Notice</Label>
+          <AlertIcon />
+          <Label
+            size="small"
+          >
+            &nbsp;
+            November 2020: Sign In issues
+            &nbsp;
+          </Label>
           <Button
             icon={<DownIcon size="small" style={{ padding: '4px' }} />}
             onClick={() => { this.setState({ expand: !expand }) }}
@@ -43,9 +53,24 @@ class GlobalNotification extends React.Component {
           />
         </Box>
         {expand && (
-          <Paragraph>
-            Full details
-          </Paragraph>
+          <Box>
+            <Paragraph margin="small" pad="small" size="small" style={{ maxWidth: 'none' }}>
+              We are currently looking into an issue where educators and
+              students cannot login to Zooniverse Classrooms via the Sign In
+              button at the top of this website.
+              We apologise for this inconvenience.
+              While we work on a long term fix, we've found that the following
+              workaround allows users to sign in to Zooniverse Classrooms:
+            </Paragraph>
+            <Paragraph margin="small" pad="small" size="small" style={{ maxWidth: 'none' }}>
+              1. Go to <Anchor href="https://www.zooniverse.org">Zooniverse.org</Anchor>,
+              click on the Sign In button, and proceed to log in.
+            </Paragraph>
+            <Paragraph margin="small" pad="small" size="small" style={{ maxWidth: 'none' }}>
+              2. Once you're logged in at Zooniverse.org, return to this
+              website and click the Sign In button.
+            </Paragraph>
+          </Box>
         )}
       </Box>
     )
