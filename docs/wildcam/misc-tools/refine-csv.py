8e79e011f1ab68f7bcae1caafc667d8416d9194f
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2021-03-22 20:38:34 +0000

    Finish Filter CSV function

--- docs/wildcam/misc-tools/refine-csv.py
+++ docs/wildcam/misc-tools/refine-csv.py
@@ -1,11 +1,17 @@
-# Refine CSV
+# Filter CSV
 # ----------
 #
 # Reads a CSV file (usually a Zooniverse Subjects export or Classifications
-# export) and filters for specific rows. 
+# export) and filters for rows with specific values.
 #
 # Usage:
-#   TODO
+#   python3 filter-csv.py inputFilename.csv outputFilename.csv columnName columnValue
+#
+# Example
+#   "Get all classifications made by user darkeshard"
+#   python3 filter-csv.py gorongosa-classifications.csv output.csv user_name darkeshard
+#
+# (@shaunanoordin 20210322)
 
 import sys, csv
 
@@ -18,9 +24,14 @@ outputFilename = sys.argv[2]
 columnName = sys.argv[3]
 columnValue = sys.argv[4]
 
-with open(inputFilename, newline='') as inputFile:
+with open(inputFilename, mode='r', newline='') as inputFile, \
+     open(outputFilename, mode='w+', newline='') as outputFile:
+  
   inputReader = csv.DictReader(inputFile)
+  outputWriter = csv.DictWriter(outputFile, fieldnames=inputReader.fieldnames)
+  outputWriter.writeheader()
+  
   for inRow in inputReader:
     if inRow[columnName] == columnValue:
+      outputWriter.writerow(inRow)
       print(inRow)
-
