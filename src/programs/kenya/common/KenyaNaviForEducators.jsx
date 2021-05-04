2adce35603a5b411205b7698257b68638f09378f
Shaun A. Noordin <shaunanoordin@users.noreply.github.com>
2021-05-04 18:09:44 +0100

    Add Wildwatch Kenya images and Talk link (#232)
    * Update Wildwatch Kenya images
    
    * Add Talk links for Wildwatch Kenya
    
    * Add missing ShareIcon
    
    * Fix broken Kenya links

--- src/programs/kenya/common/KenyaNaviForEducators.jsx
+++ src/programs/kenya/common/KenyaNaviForEducators.jsx
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import Anchor from 'grommet/components/Anchor';
 import Box from 'grommet/components/Box';
 
+import ShareIcon from 'grommet/components/icons/base/Share';
+
 function KenyaNavi(props) {
   return (
     <Box
@@ -11,11 +13,12 @@ function KenyaNavi(props) {
       direction="row"
       pad="small"
     >
-      <Anchor className="big link" path={`/wildwatch-darien-lab`}>Wildwatch Kenya Lab</Anchor>
-      <Anchor className="link" path={`/wildwatch-darien-lab/educators/intro`}>Educator Home</Anchor>
-      <Anchor className="link" path={`/wildwatch-darien-lab/educators`}>Classrooms</Anchor>
-      <Anchor className="link" path={`/wildwatch-darien-lab/educators/map`}>Explore Data</Anchor>
-      <Anchor className="link" path={`/wildwatch-darien-lab/educators/ecology`}>Ecology</Anchor>
+      <Anchor className="big link" path={`/wildwatch-kenya-lab`}>Wildwatch Kenya Lab</Anchor>
+      <Anchor className="link" path={`/wildwatch-kenya-lab/educators/intro`}>Educator Home</Anchor>
+      <Anchor className="link" path={`/wildwatch-kenya-lab/educators`}>Classrooms</Anchor>
+      <Anchor className="link" path={`/wildwatch-kenya-lab/educators/map`}>Explore Data</Anchor>
+      <Anchor className="link" path={`/wildwatch-kenya-lab/educators/ecology`}>Ecology</Anchor>
+      <Anchor className="external link" href="https://www.zooniverse.org/projects/sandiegozooglobal/wildwatch-kenya/talk/4345" target="_blank" rel="noopener noreferrer">Discuss <ShareIcon size="xsmall" /></Anchor>
     </Box>
   );
 };
