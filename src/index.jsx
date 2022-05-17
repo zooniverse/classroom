bd6219248130ea4da2ab83caa1b3e55f28aa2fc4
Jim O'Donnell <jim@zooniverse.org>
2022-05-17 16:43:27 +0100

    Migrate to Redux Toolkit (#332)
    Replace `createStore` with `configureStore` from Redux Toolkit. I've replaced the default middleware with JumpState.

--- src/index.jsx
+++ src/index.jsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 
 import { HashRouter as Router, Route } from 'react-router-dom';
 
+import { configureStore } from '@reduxjs/toolkit';
+import { CreateJumpstateMiddleware } from 'jumpstate';
 import { Provider } from 'react-redux';
 import oauth from 'panoptes-client/lib/oauth';
 import apiClient from 'panoptes-client/lib/api-client';
 
 import Main from './components/Main';
 import { config } from './lib/config';
-import configureStore from './store';
+import reducer from './ducks/reducer';
 
 import './styles/main.styl';
 
-const store = configureStore();
+const store = configureStore({ reducer, middleware: [CreateJumpstateMiddleware()] });
 
 oauth.init(config.panoptesAppId, { customRedirects: true })
   .then(() => {
